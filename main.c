8266b7d8fb168b26763fd2d0923377ffcc5eb87f
Balaram Pai <balaram151023@gmail.com>
2026-06-22 20:36:27 +0000

    Complete Image Viewer

--- main.c
+++ main.c
@@ -36,40 +36,50 @@ int main()
 	sscanf(pdimensions,"%d %d\n",&width,&height);  // So sscanf() parses through the pdimensions and takes the values and stores it in the address of width and height variables.
 	free(pdimensions);     // Now we can free this memory space as well.	
 	
-	printf("Width=%d,Height=%d\n",width,height);
-	printf("Hello World!\n");
+	printf("Width=%d , Height=%d\n",width,height);
 
 	// CreateWindow creates as object(The window) and stores in memory. 
 	// The pwindow variable of type sdl holds the address value.
 	// The *pwindow accesses the object i.e. the window and its contents from that memory.
 	// More like CreateWindow builds house at an address and pwindow is a paper on which the address is written. And when I do *pwindow Im accessing the house or using it with the pwindow address.
 	// Params : ( Title , Position in X-Axis , Position in Y-Axis , Width , Height , Flags(0 or anyother based on docs) )
-	SDL_Window *pwindow =  SDL_CreateWindow("Image Viewer",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,900,600,0);
+	SDL_Window *pwindow =  SDL_CreateWindow("Image Viewer",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,0);
 	
 	// Now we need to access the window's(Objects) drawing surface the canvas( The blank space) using the windows object address.
 	// The GetWindow function returns address into psurface. [More over its the syntax.]
 	SDL_Surface *psurface =  SDL_GetWindowSurface(pwindow);
 
-	// Unsigned Integer (8 bit) -- as colours go from 0-255 and 2**8 is 256.
-	Uint8 r,g,b;        // Ranges from 0-255 in decimal format , We shall use that instead of hexa-decimal.
-	r = 255;
-	g = b = 0;
-	
-	//Convert RBG values into the colour format used by SDL surface.
-	Uint32 color = SDL_MapRGB(psurface->format,r,g,b);       // psurface->format  is short cut for (*psurface).format
-								 // Here we have used the format of the surface but there are many predefine types like bitsperpixel,bytesperpixel etc.
-	
+		
 	// Now let us try to fill the surface with each pixel represented by a rectangle.
 	// Lets declare how a pixel looks like or is structured outside the loop to avoid decalaration in every loop.
 	// Intitiated with 0 abnd then is updated inside the loop.
 	SDL_Rect pixel = (SDL_Rect){0,0,1,1};	// Simple for :  SDL_Rect Pixel;
 						// 		 Pixel.x = 0 , pixel.y = 0 , pixel.w = 1 , pixel.h = 1;
 	
-	for(int x=0 ; x<width ; x++)
+	
+	Uint32 color = 0;  // Declaring outside the loop.
+	
+	// Now we fill each pixel.
+	for(int y=0 ; y<height ; y++)
 	{
-		for(int y=0; y<height ; y++)
+		for(int x=0; x<width ; x++)
 		{
-			pixel.x=x,pixel.y = y;                  // Update the position of each pixel as sixe is constant.
+
+		        // Unsigned Integer (8 bit) -- as colours go from 0-255 and 2**8 is 256.
+			Uint8 r,g,b;        // Ranges from 0-255 in decimal format , We shall use that instead of hexa-decimal.
+			r = (char) getchar(); 	// Reads from the file the ASCII value of the Pixel each.
+			g = (char) getchar();
+			b = (char) getchar();
+						
+			//Convert RBG values into the colour format used by SDL surface.
+			color = SDL_MapRGB(psurface->format,r,g,b);       // psurface->format  is short cut for (*psurface).format
+							                  // Here we have used the format of the surface but there are many predefine types like bitsperpixel,bytesperpixel etc.
+			
+                	// Update the position of each pixel as sixe is constant.
+			pixel.x = x;
+			pixel.y = y;
+
+
 			SDL_FillRect(psurface,&pixel,color);	// Fills the pixel in the surface with the colour.
 		}
 	}
@@ -78,6 +88,18 @@ int main()
 	SDL_UpdateWindowSurface(pwindow);
 
 	
+	int app_running = 1;
+	while (app_running) 
+	{
+    		SDL_Event event;
+    		while (SDL_PollEvent(&event)) 
+		{
+			if(event.type == SDL_QUIT)
+			{
+				app_running = 0;
+			}
 
-	SDL_Delay(3000);
+	    	}
+		SDL_Delay(100);    
+	}
 }  
